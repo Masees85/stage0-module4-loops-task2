2eb7c9b8675a43bf37e565f43e2832f9eeec6b7e
Masees85 <109299067+Masees85@users.noreply.github.com>
2022-08-15 08:04:45 -0400

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.loops.task2;
 
-public class PrimeNumbers {
-    public void printPrimeNumbers(int printToInclusive) {
+public class PrimeNumbers 
+
+{
+    public void printPrimeNumbers(int n) 
+    
+    
+    {
+        
+        
+                if (n%2==0 && n%1==0 && n!=2  )
+        {
+            System.out.println("not prime");
+        }
+        else
+        {
+            System.out.println("prime");
+            
+        }
     }
 }
