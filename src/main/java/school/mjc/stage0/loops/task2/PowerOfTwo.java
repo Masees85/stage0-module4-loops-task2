3eab7147aa44113e9fe488d4f9ea1b7fe652053e
Masees85 <109299067+Masees85@users.noreply.github.com>
2022-08-15 08:08:46 -0400

    Update PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -1,7 +1,21 @@
 package school.mjc.stage0.loops.task2;
 
-public class PowerOfTwo {
-    public void printPower(int power) {
+public class PowerOfTwo 
+
+{
+    public void printPower(int power) 
+    
+    {
+        
+         int base = 2;
+        int result = 1;
+        
+        while (power != 0) {
+            result = result * base;
+           
+            power--;
+            System.out.println(result);
+        }
 
     }
 }
